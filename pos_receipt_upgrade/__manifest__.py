1bece3d92640f03ec49ed611fad9e0f3e3731453
ArPol <armand@arpol.fr>
2024-06-01 00:02:09 +0200

    pos_receipt_upgrade simplified manifest

--- pos_receipt_upgrade/__manifest__.py
+++ pos_receipt_upgrade/__manifest__.py
@@ -3,16 +3,13 @@
 
 {
     'name': 'Amélioration du modèle de reçu du point de vente',
-    'version': '1.0.1',
-    'category': 'Sales/Point Of Sale',
-    'sequence': 40,
     'summary': '[ArPol] Module personnalisé pour Casa Angels',
+    'version': '16.0.1',
+    'category': 'Sales/Point Of Sale',    
     'depends': ['point_of_sale','l10n_fr_siret'],
     'assets': {
         'point_of_sale.assets': [
             'pos_receipt_upgrade/static/xml/OrderReceipt_inherit.xml',
         ],
     },
-    'installable': True,
-    'application': False,
 }
