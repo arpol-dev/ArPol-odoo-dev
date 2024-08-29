09f63c7c2b42ec816a9d19a99db943cc61c7f921
ArPol <armand@arpol.fr>
2024-08-29 19:03:39 +0200

    Add qweb_today_date_module

---
+++ qweb_today_date/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Amélioration du modèle de reçu du point de vente',
+    'summary': '[ArPol] Module personnalisé pour Casa Angels',
+    'version': '16.0.1',
+    'sequence': 10,
+    'category': 'Sales/Point Of Sale',    
+    'depends': ['base','point_of_sale','l10n_fr_siret','pos_loyalty'],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_receipt_upgrade/static/src/xml/OrderReceipt.xml',
+            'pos_receipt_upgrade/static/src/js/models.js'
+        ],
+    },
+}
---
+++ qweb_today_date/models/res_users.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, _
+import datetime
+import babel
+
+import logging
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    @api.model
+    def get_formatted_date(self):
+        lang = self.env.user.lang or 'en_US'
+        date = datetime.now()
+
+        formatted_date = babel.dates.format_date(date, format='dd MMMM yyyy', locale=lang)
+        return formatted_date
