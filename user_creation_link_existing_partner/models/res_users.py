8d68e477acf4b664864f1aee0c01e27e71835d01
ArPol <armand@arpol.fr>
2024-11-29 21:14:02 +0100

    Correction for accessing values['partner_id'] in res_user model

--- user_creation_link_existing_partner/models/res_users.py
+++ user_creation_link_existing_partner/models/res_users.py
@@ -6,6 +6,6 @@ class ResUsers(models.Model):
     def _signup_create_user(self, values):
         if values['email']:
             partner = self.env["res.partner"].sudo().search([('email','=',values['email'])], limit=1)
-        if partner and not values['partner_id']:
+        if partner and not values.get('partner_id'):
             values['partner_id'] = partner.id
         return super(ResUsers, self)._signup_create_user(values)
