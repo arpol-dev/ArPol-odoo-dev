7b3b3989e0b1fe719d16f75ed0f9ac6379666cbf
default <default@Legion5-15ACH6H-82JU>
2025-10-05 08:48:17 +0200

    [ADD] appointment_custom_videolocation

---
+++ appointment_custom_videolocation/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @author: Armand Polmard (contact@arpol.fr)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Appointment Custom Video Location",
+    "version": "1.0",
+    "summary": "Allow to manually define the video link for each meeting.",
+    "author": "Armand POLMARD",
+    "category": "Services",
+    "depends": ["appointment"],
+    "data": ["views/appointment_view.xml"],
+    "license": 'LGPL-3',
+    "installable": True,
+    "application": False,
+}
---
+++ appointment_custom_videolocation/models/appointment.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @author: Armand Polmard (contact@arpol.fr)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models, _
+
+class AppointmentType(models.Model):
+    _inherit = "appointment.type"
+
+    event_videocall_source = fields.Selection(
+        selection_add=[('custom', 'Custom video provider')],
+        ondelete={'custom': 'set default'}
+    )
+    custom_videocall_location = fields.Char(string="Custom Videocall Link", help="Custom video call link to be used when 'Custom video provider' is selected as video call source.")
+
+class CalendarEvent(models.Model):
+    _inherit = "calendar.event"
+
+    @api.depends('videocall_source', 'access_token')
+    def _compute_videocall_location(self):
+        super()._compute_videocall_location()
+        for event in self:
+            if event.appointment_type_id and event.appointment_type_id.custom_videocall_location and event.videocall_source == 'custom':
+                event.videocall_location = event.appointment_type_id.custom_videocall_location
+
+    @api.depends('videocall_location', 'access_token')
+    def _compute_videocall_redirection(self):
+        super()._compute_videocall_redirection()
+        for event in self:
+            if event.videocall_source == 'custom' and event.videocall_location:
+                event.videocall_redirection = event.videocall_location
