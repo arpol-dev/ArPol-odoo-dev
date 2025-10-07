eadb25012cd1be25b37ea8fea658a0faadc784bb
default <default@Legion5-15ACH6H-82JU>
2025-10-07 11:58:38 +0200

    [UPG] Add teams button in calendar.event quick_create view / Conditionnal display of the button depending on the outlook calendar synchronisation

--- microsoft_calendar_teams/models/calendar_event.py
+++ microsoft_calendar_teams/models/calendar_event.py
@@ -8,7 +8,15 @@ class Meeting(models.Model):
     _inherit = 'calendar.event'
 
     videocall_source = fields.Selection(selection_add=[('teams', 'Microsoft Teams')], ondelete={'microsoft_teams': 'set null'})
-    
+    microsoft_is_calendar_sync_enabled = fields.Boolean(string="Is Outlook Calendar Sync configured and active ?", compute='_compute_microsoft_is_calendar_sync_enabled', store=False)
+
+    def _compute_microsoft_is_calendar_sync_enabled(self):
+        user = self.env.user        
+        if user.microsoft_calendar_token and user.microsoft_calendar_token_validity and user.microsoft_calendar_token.validity > fields.Datetime.now() and not user.microsoft_synchronisation_stopped:
+            self.microsoft_is_calendar_sync_enabled = True
+        else:
+            self.microsoft_is_calendar_sync_enabled = False
+
     def set_teams_videocall_location(self):
         self.videocall_source = 'teams'
 
