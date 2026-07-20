4a5ea85b705b0fa2108eb5e82443ecb45f245dca
Armand POLMARD <armand@arpol.fr>
2026-07-20 11:09:34 +0000

    [FIX] appointment_custom_videolocation: add custom_videolink to SELF_READABLE/WRITEABLE_FIELDS
    Sans cette déclaration, le bypass d'accès propre de res.users.read() échouait
    pour tout employé accédant à son profil, bloquant la lecture des champs privés HR.

--- appointment_custom_videolocation/models/res_users.py
+++ appointment_custom_videolocation/models/res_users.py
@@ -5,3 +5,11 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     custom_videolink = fields.Char(string="Custom Video Link")
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + ['custom_videolink']
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + ['custom_videolink']
